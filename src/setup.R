35864ea4ece34b876f4be1c9f1be273e83d2c34b
Nicolas Casajus <nicolas.casajus@gmail.com>
2019-12-18 11:44:56 +0100

    Rename R Scripts

--- src/setup.R
+++ src/setup.R
@@ -1,6 +1,6 @@
 #' @title Project Settings
 #'
-#' @author Nicolas Casajus, \email{nicolas.casajus@@gmail.com}
+#' @author Nicolas Casajus, \email{nicolas.casajus@@fondationbiodiversite.fr}
 
 
 
@@ -11,7 +11,6 @@ rm(list = ls())
 #' ----------------------------------------------------------------------------- @LoadLibraries
 
 pkgs <- c(
-  "devtools",
   "readxl"
 )
 
@@ -21,37 +20,18 @@ lapply(
   dependencies = TRUE
 )
 
-if (!("emo" %in% installed.packages())) devtools::install_github("hadley/emo")
-
-
-if (sum(unlist(lapply(pkgs, require, character.only = TRUE))) == length(pkgs)) {
-
-  cat("\n", emo::ji("computer"), ">>> All packages loaded !\n")
-
-} else {
-
-  cat("\n", emo::ji("warning"), ">>> Some packages failed to load !\n")
-
-}
-
 
 
 #' ----------------------------------------------------------------------------- @CreateResultFolders
 
-script_names <- list.files(path = "src", pattern = "^[0-9]{2}.+\\.R$")
-script_names <- script_names[-1]
+script_names <- list.files(path = "src", pattern = "^.+\\.R$")
+script_names <- script_names[-which(script_names == "setup.R")]
 dir_names    <- gsub("\\.R", "", script_names)
-dir_vars     <- regmatches(
-  x = dir_names,
-  m = regexpr(
-    pattern = "^[0-9]{2}[a-z]?",
-    text = dir_names
-  )
-)
+dir_vars     <- dir_names
 
 if (length(dir_vars) > 0) {
 
-  dir_vars     <- paste0("res_dir_", dir_vars)
+  dir_vars <- paste0("res_dir_", dir_vars)
 
   sapply(1:length(dir_names), function(i) {
 
@@ -67,9 +47,6 @@ if (length(dir_vars) > 0) {
       envir  = .GlobalEnv
     )
   })
-
-  cat("\n", emo::ji("folder"), ">>> All folders created !\n")
-
 }
 
 
