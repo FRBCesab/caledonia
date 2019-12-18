1d9bca1e8d7646ea9cfe595862945921d84d4267
Nicolas Casajus <nicolas.casajus@gmail.com>
2019-12-18 11:47:09 +0100

    Update makefile + Untracked res/ folder

--- make.R
+++ make.R
@@ -1,8 +1,9 @@
 #' @title Run Project
 #'
-#' @author Nicolas Casajus, \email{nicolas.casajus@@gmail.com}
+#' @author Nicolas Casajus, \email{nicolas.casajus@@fondationbiodiversite.fr}
 
 
 
-source(file.path("src", "00_setup.R"))
-source(file.path("src", "01_fish_data_clean.R"))
+source(file.path("src", "setup.R"))
+source(file.path("src", "get_stations_coords.R"))
+source(file.path("src", "fish_data_clean.R"))
